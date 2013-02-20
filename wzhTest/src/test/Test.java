baf9e01872415ecb213591f4d5b1e61c6a2fd9b9
Administrator <Administrator@CHINA-64A287099>
2013-02-20 11:42:42 +0800

    init

---
+++ wzhTest/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String s="11";
+		System.out.println("hello world!"+s);
+ 
+	}
+
+} 
