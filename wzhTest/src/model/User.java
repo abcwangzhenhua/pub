64bcb9ddb2349353d42d7625ca3d78f3af6bb6ff
wangzhenhua <abcwangzhenhua@gmail.com>
2013-05-12 22:12:55 +0800

    hibernate3 exercise

--- wzhTest/src/model/User.java
+++ wzhTest/src/model/User.java
@@ -1,5 +1,52 @@
 package model;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
+
+
+@Entity
+@Table(name = "ts_user", catalog = "test")
+public class User {	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", unique = true, nullable = false)
+	private Integer c_id;
+	@Column(name = "c_user", length = 32)
+	private String c_user;
+	@Column(name = "`key`", length = 32)
+	private String key;
+	public User(){
+		
+	}
+	@Override
+	public String toString() {
+		return "User [c_id=" + c_id + ", c_user=" + c_user + ", key=" + key
+				+ "]";
+	}
+	public Integer getC_id() {
+		return c_id;
+	}
+	public void setC_id(Integer c_id) {
+		this.c_id = c_id;
+	}
+	public String getC_user() {
+		return c_user;
+	}
+	public void setC_user(String c_user) {
+		this.c_user = c_user;
+	}
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
 }
